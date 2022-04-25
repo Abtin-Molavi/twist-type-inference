f7f94bac23967220161477e31b02c3a014c2e659
Stephen Jasina <stephenjasina@gmail.com>
2022-04-25 17:13:54 -0400

    Start adding Twist types to AST

--- twistTyping.hs
+++ twistTyping.hs
@@ -18,27 +18,27 @@ type TyCons = (TyEx, TyEx)
 data StTy = Pure | Mixed
     deriving (Show, Eq)
 
-data TwEx = 
-            QInit | Var String | FunVar String 
-            | U1 TwEx | U2 TwEx 
-            | LetEx TwEx TwEx TwEx  |  App TwEx TwEx | Pair TwEx 
-            | ITE TwEx TwEx TwEx | TwT | TwF | Msr TwEx 
-            | MkEnt StTy TwEx | Split StTy TwEx | Cast StTy TwEx
+data TwEx =
+            QInit Maybe TwTy | Var String Maybe TwTy | FunVar String Maybe TwTy
+            | U1 TwEx Maybe TwTy | U2 TwEx Maybe TwTy
+            | LetEx TwEx TwEx TwEx Maybe TwTy  |  App TwEx TwEx Maybe TwTy | Pair TwEx Maybe TwTy
+            | ITE TwEx TwEx TwEx Maybe TwTy| TwT Maybe TwTy | TwF Maybe TwTy | Msr TwEx Maybe TwTy
+            | MkEnt StTy TwEx Maybe TwTy | Split StTy TwEx Maybe TwTy | Cast StTy TwEx Maybe TwTy
 
-data TwProg = Fun String String TwEx TwProg | Main TwEx 
+data TwProg = Fun String String TwEx TwProg Maybe TwTy | Main TwEx Maybe TwTy
 
 -- (foldl Set.union Set.empty (map (\x -> Set.union (freeVars $ fst x)  (freeVars $ snd x))  (genConstraints expr)))
 
-genConstraints :: Set.Set TyVar -> TwEx -> [TyCons]
-genConstraints currentVars (LetEx lhs rhs expr) = (PlainVar lty, PlainVar rty) : genConstraints (Set.union (Set.fromList [lty, rty]) currentVars) expr
-     where [lty, rty] = freshVars 2 currentVars
-genConstraints currentVars QInit = []
-genConstraints currentVars (U1 expr) = (PlainVar tyv, Exactly (Mixed Qubit)) : genConstraints (Set.union (Set.fromList [tyv]) currentVars) expr
-    where [tyv] = freshVars 1 currentVars
+-- genConstraints :: Set.Set TyVar -> TwEx -> [TyCons]
+-- genConstraints currentVars (LetEx lhs rhs expr) = (PlainVar lty, PlainVar rty) : genConstraints (Set.union (Set.fromList [lty, rty]) currentVars) expr
+--      where [lty, rty] = freshVars 2 currentVars
+-- genConstraints currentVars QInit = []
+-- genConstraints currentVars (U1 expr) = (PlainVar tyv, Exactly (Mixed Qubit)) : genConstraints (Set.union (Set.fromList [tyv]) currentVars) expr
+--     where [tyv] = freshVars 1 currentVars
 
 solveConstraints :: [TyCons] -> Maybe (Map.Map TyEx TyEx)
 solveConstraints [] = Just Map.empty
-solveConstraints (eq : rest) = 
+solveConstraints (eq : rest) =
     case eq of
         (lhs, rhs) | lhs == rhs -> solveConstraints rest
         (PlainVar x, rhs) | not $ Set.member x (freeVars rhs) ->  Map.union <$> Just (Map.singleton (PlainVar x) rhs) <*> solveConstraints (map (tMap (subst (PlainVar x) rhs)) rest)
@@ -51,11 +51,11 @@ solveConstraints (eq : rest) =
 freeVars :: TyEx -> Set.Set TyVar
 freeVars (PlainVar var) = Set.singleton var
 freeVars (Exactly _) = Set.empty
-freeVars (FuncEx left right) = Set.union (freeVars left) (freeVars right) 
-freeVars (ProdEx left right) = Set.union (freeVars left) (freeVars right) 
+freeVars (FuncEx left right) = Set.union (freeVars left) (freeVars right)
+freeVars (ProdEx left right) = Set.union (freeVars left) (freeVars right)
 
 subst :: TyEx -> TyEx -> TyEx -> TyEx
-subst before after expr | expr == before = after 
+subst before after expr | expr == before = after
 subst before after (FuncEx ty1 ty2) = FuncEx (subst before after ty1) (subst before after ty2)
 subst before after (ProdEx ty1 ty2) = ProdEx (subst before after ty1) (subst before after ty2)
 subst  _ _ expr = expr
@@ -63,9 +63,9 @@ subst  _ _ expr = expr
 tMap :: (a->b) -> (a,a) -> (b,b)
 tMap f (x,y) = (f x, f y)
 
-freshVars :: Int -> Set.Set TyVar -> [TyVar] 
+freshVars :: Int -> Set.Set TyVar -> [TyVar]
 freshVars n tyVars = map TyVar (take n [s | s <- allStringsLetters, not (Set.member s strs)])
-    where 
+    where
         extractStr (TyVar x) = x
         strs = Set.union (Set.singleton "") (Set.map extractStr tyVars)
-        allStringsLetters = [c : s | s <- "" : allStringsLetters, c <- ['a'..'z']] 
+        allStringsLetters = [c : s | s <- "" : allStringsLetters, c <- ['a'..'z']]
